2036a802b523c082d8cab5772317322378ddf622
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-11-27 20:17:54 +0530

    list todos

--- src/view/TodoApp.js
+++ src/view/TodoApp.js
@@ -1,12 +1,27 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, {useState} from "react";
+import {View, Text, StyleSheet, FlatList} from "react-native";
 import Header from "../components/Header";
 
 const TodoApp = () => {
-
+  const [todos, setTodos] = useState(
+    [
+      {text: 'buy coffee', key: 1},
+      {text: "create an web app", key: 2},
+      {text: "play", key: 3},
+      {text: 'buy coffee', key: 4},
+    ]
+  );
   return (
     <View style={styles.container}>
       <Header/>
+      <View style={styles.list}>
+              <FlatList
+                data={todos}
+                renderItem={({item}) => (
+                  <Text>{item.text}</Text>
+                )}
+              />
+        </View>
     </View>
   )
 }
