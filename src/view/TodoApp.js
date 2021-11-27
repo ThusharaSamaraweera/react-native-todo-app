83fc7fa8bf078e568f2ed81f75175a641ad7214f
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-11-27 20:12:11 +0530

    initize todo app

---
+++ src/view/TodoApp.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+const TodoApp = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default TodoApp;
